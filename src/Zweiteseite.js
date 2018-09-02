215c178656d77bf9e7c1c98a7717fb39404044c4
Binjam Gebre <info@react-entwickler.de>
2018-09-02 19:46:26 +0200

    etwas besser

--- src/Zweiteseite.js
+++ src/Zweiteseite.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 
+//react router
+import { withRouter } from "react-router-dom";
+
+import {connect} from 'react-redux';
+import {actionCreators} from './redux/MeineActions';
+
 export class Zweiteseite extends Component {
 
  //Hier sage ich mit componentDidMount, das wenn der Client nicht der user ist soll er auf die erste Seite landen
@@ -25,4 +31,15 @@ export class Zweiteseite extends Component {
   }
 }
 
-export default Zweiteseite
+//Hier kommen die Zustände aus MeinStore.js rein um diese dann mit props zu verwenden
+function mapStateToProps (state) {
+  return {
+      email: state.email, 
+      password:state.password,
+      navigationEinblenden:state.navigationEinblenden
+  }
+}
+
+//wichtig: ich musste heir SignIn eintragen anstatt (App)
+
+export default withRouter(connect(mapStateToProps, actionCreators)(Zweiteseite))
