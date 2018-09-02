fe0cdbbf69330f7667d35a489aa77cf49d186d4a
Binjam Gebre <info@react-entwickler.de>
2018-09-02 23:29:50 +0200

    etwas besser

--- src/Zweiteseite.js
+++ src/Zweiteseite.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 
+//react router
+import { withRouter } from "react-router-dom";
 
+import {connect} from 'react-redux';
+import {actionCreators} from './redux/MeineActions';
 
 export class Zweiteseite extends Component {
 
@@ -27,4 +31,15 @@ export class Zweiteseite extends Component {
   }
 }
 
-export default Zweiteseite;
+//Hier kommen die Zustände aus MeinStore.js rein um diese dann mit props zu verwenden
+function mapStateToProps (state) {
+  return {
+      email: state.email, 
+      password:state.password,
+      navigationEinblenden:state.navigationEinblenden
+  }
+}
+
+//wichtig: ich musste heir SignIn eintragen anstatt (App)
+
+export default withRouter(connect(mapStateToProps, actionCreators)(Zweiteseite))
--- src/routes.js
+++ src/routes.js
@@ -6,22 +6,22 @@ import Startseite from './Startseite';
 import Zweiteseite from './Zweiteseite';
 //Die Navigation
  import Navigation from './Navigation';
-
+import { connect } from 'react-redux';
 
 class Routes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-        	loggedIn: JSON.parse(localStorage.getItem("react-localStorage-user")) ? true : false
+        	loggedIn: props.navigationEinblenden
         }
     }
 
     render() {
-    const { loggedIn } = this.state;
+    const { istAusgeloggt } = this.props;
         return (
             <HashRouter>
             	<div>
-				    { loggedIn && <Navigation/>}
+				    <Navigation/>
 				    <Route exact={true} path={"/"} component={Loginseite}/>
 				    <Route exact={true} path={"/Startseite"} component={Startseite}/>
 				    <Route exact={true} path={"/Zweiteseite"} component={Zweiteseite}/>		    
@@ -30,5 +30,11 @@ class Routes extends Component {
         );
     }
 }
-
-export default Routes;
+function mapStateToProps (state) {
+    console.log(state)
+    return {
+        istAusgeloggt:state.istAusgeloggt,
+        navigationEinblenden:state.navigationEinblenden
+    }
+}
+export default connect(mapStateToProps)(Routes);
